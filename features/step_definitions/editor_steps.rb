372cdb89528e006eb4415f2a303336baec0aee2a
André Wendt <awendt@freeshell.org>
2013-02-28 10:35:34 +0100

    Editing a disabled config file includes it in the next run

--- features/step_definitions/editor_steps.rb
+++ features/step_definitions/editor_steps.rb
@@ -1,3 +1,8 @@
 When /^I set env variable "(\w+)" to "([^"]*)"$/ do |var, value|
   ENV[var] = value
 end
+
+When /^I poet\-edit file "([^"]*)" and change something$/ do |filename|
+  ENV['EDITOR'] = "sed -i '' G"
+  step "I run `poet edit #{filename}`"
+end
