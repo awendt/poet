f9f44a5b4125e0b871ae368434a366c0742cbb44
André Wendt <awendt@freeshell.org>
2013-08-09 22:49:23 +0200

    Use a custom editor for changing files in tests
    sed's arguments do not work cross-platform

--- features/step_definitions/editor_steps.rb
+++ features/step_definitions/editor_steps.rb
@@ -3,7 +3,7 @@ When /^I set env variable "(\w+)" to "([^"]*)"$/ do |var, value|
 end
 
 When /^I poet\-edit file "([^"]*)" and change something$/ do |filename|
-  ENV['EDITOR'] = "sed -i '' G"
+  ENV['EDITOR'] = File.expand_path('../../../editors/double_space.rb', __FILE__)
   step "I run `poet edit #{filename}`"
 end
 
