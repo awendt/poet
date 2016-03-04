cd7e00ca079c3468f24fdbf778289b1aaeaef7ca
André Wendt <awendt@freeshell.org>
2016-03-04 12:36:43 +0100

    Shorten line to tame the hound

--- features/step_definitions/editor_steps.rb
+++ features/step_definitions/editor_steps.rb
@@ -1,5 +1,6 @@
 When /^I poet\-edit file "([^"]*)" and change something$/ do |filename|
-  step %Q(I set the environment variable "EDITOR" to "#{File.expand_path('../../../editors/double_space.rb', __FILE__)}")
+  double_space = File.expand_path('../../../editors/double_space.rb', __FILE__)
+  step %(I set the environment variable "EDITOR" to "#{double_space}")
   step "I run `poet edit #{filename}`"
 end
 
